a8075b3fbc91c5bcb7d6129fa6d1b501849f9ccc
xuulu <xuulu04@gmail.com>
2025-10-26 16:13:20 +0800

    优化代码

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from script.batch_download_files import batch_download_files
 import asyncio
 
 
+# 批量下载文件
+async def main_for_batch_download_files():
+    # 批量下载文件(读取指定文件，一行一个url，并发下载到指定目录)
+    results = batch_download_files(
+        url_file_path=r"C:\Users\a\test.txt",  # 链接文件路径
+        download_dir=r"C:\Users\a\downloads",  # 下载目录
+        max_workers=4  # 并发下载数
+    )
+
+
 # 图片数据集管理
 async def main_for_image_dataset_manager():
     # 创建管理器实例
@@ -11,7 +21,7 @@ async def main_for_image_dataset_manager():
         root_dir=r"./images",  # 要保存的根目录
         per_dir_limit=500,  # 每个子目录文件上限
         move=False,  # False=复制, True=移动
-        workers=4  # 并发工作进程数
+        workers=8  # 并发工作进程数
     )
 
     # 添加图像（支持单文件或目录）
@@ -28,27 +38,25 @@ async def main_for_video_dataset_manager():
         root_dir="./videos",  # 要保存的根目录
         per_dir_limit=200,  # 每个子目录文件上限
         move=False,  # False=复制, True=移动
-        workers=4  # 并发工作进程数
+        workers=8  # 并发工作进程数
     )
 
     # 添加视频（可传入单个视频或目录
-    await manager.add_videos(r"C:\Users\a\Downloads\downloads")
+    await manager.add_videos(r"C:\Users\a\Downloads")
 
     # 释放资源
     manager.close()
 
 
-async def main_for_batch_download_files():
-    # 批量下载文件(读取指定文件，一行一个url，并发下载到指定目录)
-    results = batch_download_files(
-        url_file_path=r"C:\Users\a.txt",  # 链接文件路径
-        download_dir=r"C:\Users\a\downloads",  # 下载目录
-        max_workers=8  # 并发下载数
-    )
-    print(results)
-
 if __name__ == "__main__":
+    # 批量下载文件
+    # asyncio.run(main_for_batch_download_files())
+
     # 图片数据集管理
     # asyncio.run(main_for_image_dataset_manager())
+
     # 视频数据集管理
-    asyncio.run(main_for_video_dataset_manager())
+    # asyncio.run(main_for_video_dataset_manager())
+
+    pass
+
